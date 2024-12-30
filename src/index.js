ae6cd09687c74aa5482ad3c5caf345b055e1fc4f
Sufiyan <suffynux@gmail.com>
2024-12-30 18:00:58 +0500

    Initial commit without .gitignore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { app } from "./app.js";
+import dotenv from "dotenv";
 
-const port = 8000;
-app.listen (port , () => {
-    console.log(`App is running on the ${port}`);
-    
-})
+dotenv.config({
+  path: "./.env"
+});
+
+const port = process.env.PORT || 2000; // Set a default port if not found in .env
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
