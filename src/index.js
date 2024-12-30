d2dbc785a8aacae16c37d7e7822d9d5901e9902a
Sufiyan <suffynux@gmail.com>
2024-12-30 13:45:16 +0500

    Express installed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-console.log("Backend learning");
-console.log("Backend learning");
+import { app } from "./app.js";
 
+const port = 8000;
+app.listen (port , () => {
+    console.log(`App is running on the ${port}`);
+    
+})
