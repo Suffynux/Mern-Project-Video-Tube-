82c2f7f2a2999a0ce29ef1d8d52eec58d4749a4d
Sufiyan <suffynux@gmail.com>
2025-01-20 17:33:57 +0500

    Video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,30 +1,50 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        require : true,
-        unique : true,
-        lowercase : true,   //convert to lowercase
-        trim : true,     //remove white space
-        index : true,   //unable your field for searching
+      type: String,
+      require: true,
+      unique: true,
+      lowercase: true, //convert to lowercase
+      trim: true, //remove white space
+      index: true, //unable your field for searching
     },
-    email : {
-        type: String,
-        require : true,
-        unique : true,
-        lowercase : true,   //convert to lowercase
-        trim : true,     //remove white space
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+      lowercase: true, //convert to lowercase
+      trim: true, //remove white space
     },
-    fullName :{
-        type: String,
-        require : true,
-        trim : true,     //remove white space
-        index : true
+    fullName: {
+      type: String,
+      require: true,
+      trim: true, //remove white space
+      index: true,
     },
-    avatar : {
-        type: String, // We will use the online cloud service
-    }
-})
+    avatar: {
+      type: String, // We will use the online cloud service
+      require: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      require: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
 
-export const User = mongoose.model("User" , userSchema); 
+export const User = mongoose.model("User", userSchema);
