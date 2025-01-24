87ed95ea4ebb62ba674b56337d95b7a26caf762c
Sufiyan <suffynux@gmail.com>
2025-01-24 19:19:55 +0500

    Refresh token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import bcyrpt from "bcrypt";
-import jsonwebtoken from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -49,18 +49,43 @@ const userSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
-
-// Mongo db hook(middleware) for 
-userSchema.pre("save" , async function(next){
+// Mongo db hook(middleware) for
+userSchema.pre("save", async function (next) {
   let passwordSalt = 10;
-  if(!this.isModified("password")) return next();
-  this.password = bcyrpt.hash(this.password , passwordSalt);
-  next()
-})
+  if (!this.isModified("password")) return next();
+  this.password = bcyrpt.hash(this.password, passwordSalt);
+  next();
+});
 
 // Custom Moongoose Method for Password check
 userSchema.methods.isPasswordCorrect = async function (password) {
-  let checkedPassword = await bcyrpt.compare(password, this.password );
+  let checkedPassword = await bcyrpt.compare(password, this.password);
   return checkedPassword;
-}
+};
+
+userSchema.methods.generateAccessToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_KEY,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  )
+
+};
+userSchema.methods.generateRefreshToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_KEY,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  )
+};
 export const User = mongoose.model("User", userSchema);
