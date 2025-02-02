0c7e4c457b50cccba0d3173c49370bd63f24e2b5
Sufiyan <suffynux@gmail.com>
2025-02-02 14:48:29 +0500

    User registration added

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(cors(
         origin : process.env.CORS_ORIGIN,
         credentials : true
     }
-))
+))  
 // Middle for accepting Json Data
 app.use(express.json({limit : "16kb"}))
 app.use(express.urlencoded({extended : true , limit: "16kb"}));
@@ -21,6 +21,5 @@ import userRouter from "./routes/user.routes.js";
 
 // route declaration
 app.use("/api/v1/users" , userRouter)
-
 
 export {app}
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,18 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async(req , res) =>{
-    res.status(200).json({message : "ok"})
+    // get users details from Frontend/Postman
+    // Validation - Not empty
+    // Check if user is already is exists : username and email 
+    // Check for images , check for avatar
+    // If images and avatar exists then send to cloudinary
+    // Create user object - create entry in db
+    // check user creation
+    // return response
+
+    const {username , email , fullName} = req.body;
+    console.log(username , email , fullName) 
+
 }
 )
 
