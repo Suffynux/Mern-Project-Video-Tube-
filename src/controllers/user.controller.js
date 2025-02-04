e2d415e6ba6df04b5a55fb83f6a3ea96a8b0f1ea
Sufiyan <suffynux@gmail.com>
2025-02-04 18:27:27 +0500

    Validation added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
 
 const registerUser = asyncHandler(async(req , res) =>{
     // get users details from Frontend/Postman
@@ -10,10 +11,32 @@ const registerUser = asyncHandler(async(req , res) =>{
     // check user creation
     // return response
 
-    const {username , email , fullName} = req.body;
-    console.log(username , email , fullName) 
+    const {username , email , fullName , password} = req.body;
+    
+    if (username === ""){
+        throw new ApiError(400 , "Username is required");
+    }
 
+    if(email === ""){
+        throw new ApiError(400 , "email is required")
+    } 
+
+    // validation for email
+    if(email.includes("@")) {
+        console.log("Email is valid");
+    } else {
+        throw new ApiError(400 , "Email is not valid");
+        
+    }
+
+    if(fullName === ""){
+        throw new ApiError(400 , "FullName is required")
+    }
+
+    if(password === ""){
+        throw new ApiError(400 , "password is required")
+    }
 }
-)
+);
 
 export {registerUser};
