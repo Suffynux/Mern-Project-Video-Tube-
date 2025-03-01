a995ebb2191f6c4df79ab0b1d60e79cb34573274
Sufiyan <suffynux@gmail.com>
2025-03-01 18:11:10 +0500

    Toggle subscription functionality

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import {Subscription} from "../models/subscription.model.js"
+import { subscribe } from "diagnostics_channel";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
@@ -19,15 +20,16 @@ const toggleSubscription = asyncHandler(async (req, res) => {
     }
 
     const existedSubscriber = await Subscription.findOne({
-        subcriber : userId,
+        subscriber : userId,
         channel : channelId
     })
-
+    console.log(existedSubscriber);
+    
     if(existedSubscriber){
         await Subscription.findByIdAndDelete(existedSubscriber._id)
         return res.status(200).json(new ApiResponse(200, null, "Unsubscribed Successfully"))
     } else {
-        const newSubscription = Subscription.create({
+        const newSubscription = await Subscription.create({
             subscriber : userId,
             channel : channelId,
         })
